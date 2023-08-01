885fd5a321d082401cf867aab2b066fd0ccb3445
zhouxianhong <zhouxianhong@huawei.com>
2023-08-01 19:12:59 +0800

    feat(vpc): support vpc resource

---
+++ huaweicloudstack/services/vpc/data_source_hcs_vpc_subnets.go
@@ -0,0 +1,242 @@
+package vpc
+
+import (
+	"context"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/huaweicloud/terraform-provider-hcs/huaweicloudstack/config"
+	"github.com/huaweicloud/terraform-provider-hcs/huaweicloudstack/helper/hashcode"
+	"github.com/huaweicloud/terraform-provider-hcs/huaweicloudstack/sdk/huaweicloud/openstack/common/tags"
+	"github.com/huaweicloud/terraform-provider-hcs/huaweicloudstack/sdk/huaweicloud/openstack/networking/v1/subnets"
+	"github.com/huaweicloud/terraform-provider-hcs/huaweicloudstack/utils"
+)
+
+func DataSourceVpcSubnets() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceVpcSubnetsRead,
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"cidr": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"vpc_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"gateway_ip": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"primary_dns": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"secondary_dns": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"availability_zone": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"tags": {
+				Type:     schema.TypeMap,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"subnets": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"cidr": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"status": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"vpc_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"gateway_ip": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"primary_dns": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"secondary_dns": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"availability_zone": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"dhcp_enable": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+						"dns_list": {
+							Type:     schema.TypeSet,
+							Computed: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+							Set:      schema.HashString,
+						},
+						"subnet_id": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "schema: Deprecated",
+						},
+						"ipv4_subnet_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"ipv6_subnet_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"ipv6_enable": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+						"ipv6_cidr": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"ipv6_gateway": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"description": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"tags": {
+							Type:     schema.TypeMap,
+							Computed: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceVpcSubnetsRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*config.Config)
+	region := config.GetRegion(d)
+	client, err := config.NetworkingV1Client(region)
+	if err != nil {
+		return diag.Errorf("error creating VPC client: %s", err)
+	}
+
+	clientV2, err := config.NetworkingV2Client(region)
+	if err != nil {
+		return diag.Errorf("error creating VPC V2 client: %s", err)
+	}
+
+	listOpts := subnets.ListOpts{
+		ID:               d.Get("id").(string),
+		Name:             d.Get("name").(string),
+		CIDR:             d.Get("cidr").(string),
+		Status:           d.Get("status").(string),
+		GatewayIP:        d.Get("gateway_ip").(string),
+		PRIMARY_DNS:      d.Get("primary_dns").(string),
+		SECONDARY_DNS:    d.Get("secondary_dns").(string),
+		AvailabilityZone: d.Get("availability_zone").(string),
+		VPC_ID:           d.Get("vpc_id").(string),
+	}
+
+	subnetList, err := subnets.List(client, listOpts)
+	if err != nil {
+		return diag.Errorf("unable to retrieve subnets: %s", err)
+	}
+
+	log.Printf("[DEBUG] Retrieved subnets using given filter: %+v", subnetList)
+
+	var subnets []map[string]interface{}
+	tagFilter := d.Get("tags").(map[string]interface{})
+	var ids []string
+	for _, item := range subnetList {
+		subnet := map[string]interface{}{
+			"id":                item.ID,
+			"name":              item.Name,
+			"description":       item.Description,
+			"cidr":              item.CIDR,
+			"status":            item.Status,
+			"gateway_ip":        item.GatewayIP,
+			"dns_list":          item.DnsList,
+			"ipv6_enable":       item.EnableIPv6,
+			"dhcp_enable":       item.EnableDHCP,
+			"primary_dns":       item.PRIMARY_DNS,
+			"secondary_dns":     item.SECONDARY_DNS,
+			"availability_zone": item.AvailabilityZone,
+			"vpc_id":            item.VPC_ID,
+			"subnet_id":         item.SubnetId,
+			"ipv4_subnet_id":    item.SubnetId,
+			"ipv6_subnet_id":    item.IPv6SubnetId,
+			"ipv6_cidr":         item.IPv6CIDR,
+			"ipv6_gateway":      item.IPv6Gateway,
+		}
+
+		if resourceTags, err := tags.Get(clientV2, "subnets", item.ID).Extract(); err == nil {
+			tagmap := utils.TagsToMap(resourceTags.Tags)
+
+			if !utils.HasMapContains(tagmap, tagFilter) {
+				continue
+			}
+			subnet["tags"] = tagmap
+		} else {
+			return diag.Errorf("error query tags of subnets (%s): %s", item.ID, err)
+		}
+
+		subnets = append(subnets, subnet)
+		ids = append(ids, item.ID)
+	}
+	log.Printf("[DEBUG] Subnets List after filter, count=%d :%+v", len(subnets), subnets)
+
+	mErr := d.Set("subnets", subnets)
+	if mErr != nil {
+		return diag.Errorf("set subnets err:%s", mErr)
+	}
+
+	d.SetId(hashcode.Strings(ids))
+
+	return nil
+}
---
+++ huaweicloudstack/services/vpc/resource_hcs_networking_vip.go
@@ -0,0 +1,283 @@
+package vpc
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+
+	golangsdk "github.com/huaweicloud/terraform-provider-hcs/huaweicloudstack/sdk/huaweicloud"
+
+	"github.com/huaweicloud/terraform-provider-hcs/huaweicloudstack/common"
+	"github.com/huaweicloud/terraform-provider-hcs/huaweicloudstack/config"
+	"github.com/huaweicloud/terraform-provider-hcs/huaweicloudstack/sdk/huaweicloud/openstack/networking/v1/ports"
+	"github.com/huaweicloud/terraform-provider-hcs/huaweicloudstack/sdk/huaweicloud/openstack/networking/v1/subnets"
+	"github.com/huaweicloud/terraform-provider-hcs/huaweicloudstack/utils"
+)
+
+func ResourceNetworkingVip() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceNetworkingVipCreate,
+		ReadContext:   resourceNetworkingVipRead,
+		UpdateContext: resourceNetworkingVipUpdate,
+		DeleteContext: resourceNetworkingVipDelete,
+
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Timeouts: &schema.ResourceTimeout{
+			Create: schema.DefaultTimeout(2 * time.Minute),
+			Delete: schema.DefaultTimeout(2 * time.Minute),
+		},
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"network_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"ip_version": {
+				Type:         schema.TypeInt,
+				Optional:     true,
+				ForceNew:     true,
+				Computed:     true,
+				ValidateFunc: validation.IntInSlice([]int{4, 6}),
+			},
+			"ip_address": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Computed: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			// Computed
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"device_owner": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"mac_address": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+
+			// Deprecated
+			"subnet_id": {
+				Type:          schema.TypeString,
+				Optional:      true,
+				ForceNew:      true,
+				Computed:      true,
+				ConflictsWith: []string{"ip_version"},
+				Deprecated:    "use ip_version instead",
+			},
+		},
+	}
+}
+
+func resourceNetworkingVipCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*config.Config)
+	client, err := config.NetworkingV1Client(config.GetRegion(d))
+	if err != nil {
+		return diag.Errorf("error creating VPC network v1 client: %s", err)
+	}
+
+	networkId := d.Get("network_id").(string)
+	n, err := subnets.Get(client, networkId).Extract()
+	if err != nil {
+		return diag.Errorf("error retrieving subnet by network ID (%s): %s", networkId, err)
+	}
+
+	// Check whether the subnet ID entered by the user belongs to the same subnet as the network ID.
+	subnetId := d.Get("subnet_id").(string)
+	if subnetId != "" && subnetId != n.SubnetId && subnetId != n.IPv6SubnetId {
+		return diag.Errorf("the subnet ID does not belong to the subnet where the network ID is located.")
+	}
+
+	// Pre-check for subnet network, the virtual IP of IPv6 must be established on the basis that the subnet supports
+	// IPv6.
+	if d.Get("ip_version").(int) == 6 {
+		if n.IPv6SubnetId == "" {
+			return diag.Errorf("the subnet does not support IPv6, please enable IPv6 first.")
+		}
+		subnetId = n.IPv6SubnetId
+	} else {
+		subnetId = n.SubnetId
+	}
+
+	opts := ports.CreateOpts{
+		Name:        d.Get("name").(string),
+		DeviceOwner: "neutron:VIP_PORT",
+		NetworkId:   networkId,
+		FixedIps: []ports.FixedIp{
+			{
+				SubnetId:  subnetId,
+				IpAddress: d.Get("ip_address").(string),
+			},
+		},
+	}
+
+	log.Printf("[DEBUG] Updating network VIP (%s) with options: %#v", d.Id(), opts)
+	vip, err := ports.Create(client, opts)
+	if err != nil {
+		return diag.Errorf("error creating network VIP: %s", err)
+	}
+	log.Printf("[DEBUG] Waiting for network VIP (%s) to become available.", vip.ID)
+	d.SetId(vip.ID)
+
+	stateConf := &resource.StateChangeConf{
+		Pending:    []string{"BUILD"},
+		Target:     []string{"DOWN", "ACTIVE"},
+		Refresh:    waitForNetworkVipStateRefresh(client, vip.ID),
+		Timeout:    d.Timeout(schema.TimeoutCreate),
+		Delay:      3 * time.Second,
+		MinTimeout: 3 * time.Second,
+	}
+
+	_, err = stateConf.WaitForStateContext(ctx)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return resourceNetworkingVipRead(ctx, d, meta)
+}
+
+func setVipNetworkParams(d *schema.ResourceData, port *ports.Port) error {
+	if len(port.FixedIps) > 0 {
+		addr := port.FixedIps[0].IpAddress
+		var ipVersion int
+		if utils.IsIPv4Address(addr) {
+			ipVersion = 4
+		} else {
+			ipVersion = 6
+		}
+		mErr := multierror.Append(nil,
+			d.Set("ip_address", addr),
+			d.Set("subnet_id", port.FixedIps[0].SubnetId),
+			d.Set("ip_version", ipVersion),
+		)
+		return mErr.ErrorOrNil()
+	}
+	return nil
+}
+
+// For VIP ports, the status will always be 'DOWN'.
+func parseNetworkVipStatus(status string) string {
+	if status == "DOWN" {
+		return "ACTIVE"
+	}
+	return status
+}
+
+func resourceNetworkingVipRead(_ context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*config.Config)
+	region := config.GetRegion(d)
+	client, err := config.NetworkingV1Client(region)
+	if err != nil {
+		return diag.Errorf("error creating VPC network v1 client: %s", err)
+	}
+
+	vip, err := ports.Get(client, d.Id())
+	if err != nil {
+		return common.CheckDeletedDiag(d, err, "VPC network VIP")
+	}
+
+	log.Printf("[DEBUG] Retrieved VIP %s: %+v", d.Id(), vip)
+
+	mErr := multierror.Append(nil,
+		d.Set("region", region),
+		d.Set("name", vip.Name),
+		d.Set("status", parseNetworkVipStatus(vip.Status)),
+		d.Set("device_owner", vip.DeviceOwner),
+		d.Set("mac_address", vip.MacAddress),
+		d.Set("network_id", vip.NetworkId),
+		setVipNetworkParams(d, vip),
+	)
+
+	return diag.FromErr(mErr.ErrorOrNil())
+}
+
+func resourceNetworkingVipUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*config.Config)
+	client, err := config.NetworkingV1Client(config.GetRegion(d))
+	if err != nil {
+		return diag.Errorf("error creating VPC network v1 client: %s", err)
+	}
+
+	opts := ports.UpdateOpts{
+		Name: d.Get("name").(string),
+	}
+	log.Printf("[DEBUG] Updating network VIP (%s) with options: %#v", d.Id(), opts)
+
+	_, err = ports.Update(client, d.Id(), opts)
+	if err != nil {
+		return diag.Errorf("error updating networking VIP: %s", err)
+	}
+
+	return resourceNetworkingVipRead(ctx, d, meta)
+}
+
+func resourceNetworkingVipDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	config := meta.(*config.Config)
+	client, err := config.NetworkingV1Client(config.GetRegion(d))
+	if err != nil {
+		return diag.Errorf("error creating VPC network v1 client: %s", err)
+	}
+
+	err = ports.Delete(client, d.Id()).ExtractErr()
+	if err != nil {
+		return diag.Errorf("error deleting Network VIP: %s", err)
+	}
+
+	stateConf := &resource.StateChangeConf{
+		Pending:    []string{"DOWN", "ACTIVE"},
+		Target:     []string{"DELETED"},
+		Refresh:    waitForNetworkVipStateRefresh(client, d.Id()),
+		Timeout:    d.Timeout(schema.TimeoutDelete),
+		Delay:      3 * time.Second,
+		MinTimeout: 3 * time.Second,
+	}
+
+	_, err = stateConf.WaitForStateContext(ctx)
+	if err != nil {
+		return diag.Errorf("error deleting Network VIP: %s", err)
+	}
+
+	d.SetId("")
+
+	return nil
+}
+
+func waitForNetworkVipStateRefresh(networkingClient *golangsdk.ServiceClient, vipId string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		resp, err := ports.Get(networkingClient, vipId)
+		if err != nil {
+			if _, ok := err.(golangsdk.ErrDefault404); ok {
+				log.Printf("[DEBUG] The network VIP (%s) has been deleted.", vipId)
+				return resp, "DELETED", nil
+			}
+			return nil, "ERROR", err
+		}
+		log.Printf("[DEBUG] The status of the network VIP is: %s", resp.Status)
+		return resp, resp.Status, nil
+	}
+}
