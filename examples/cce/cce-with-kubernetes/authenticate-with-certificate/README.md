d049af4e1c8a8cbfc56870df8565f89851059e8c
xieyichao <xieyichao@huawei.com>
2023-09-07 14:58:06 +0800

    docs(cce): update cce docs && fix some links

--- examples/cce/cce-with-kubernetes/authenticate-with-certificate/README.md
+++ examples/cce/cce-with-kubernetes/authenticate-with-certificate/README.md
@@ -5,6 +5,6 @@ then we will use the Kubernetes provider to connect and manage the cluster.
 We can authenticate to the cluster with certificate.
 
 For more information about the HuaweiCloudStack provider,
-[please see document](https://registry.terraform.io/providers/huaweicloud/huaweicloud/latest/docs).
+[please see document](https://registry.terraform.io/providers/huaweicloud/hcs/latest/docs).
 For more information about the Kubernetes provider,
 [please see document](https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs).
--- examples/cce/cce-with-kubernetes/authenticate-with-certificate/main.tf
+++ examples/cce/cce-with-kubernetes/authenticate-with-certificate/main.tf
@@ -1,59 +1,50 @@
+resource "hcs_vpc" "myvpc" {
+  name = var.vpc_name
+  cidr = var.vpc_cidr
+}
+
+resource "hcs_vpc_subnet" "mysubnet" {
+  name       = var.subnet_name
+  cidr       = var.subnet_cidr
+  gateway_ip = var.subnet_gateway
+
+  # dns is required for cce node installing
+  primary_dns   = var.primary_dns
+  secondary_dns = var.secondary_dns
+  vpc_id        = hcs_vpc.myvpc.id
+}
+
 data "hcs_availability_zones" "myaz" {}
 
-data "hcs_vpc" "myvpc" {
-  id = var.vpc_id
+resource "hcs_ecs_compute_keypair" "mykeypair" {
+  name = var.key_pair_name
+}
+resource "hcs_cce_cluster" "mycce" {
+  name                   = var.cce_cluster_name
+  flavor_id              = var.cce_cluster_flavor
+  vpc_id                 = hcs_vpc.myvpc.id
+  subnet_id              = hcs_vpc_subnet.mysubnet.id
+  container_network_type = "overlay_l2"
+  cluster_type           = "ARM64"
+  eip                    = hcs_vpc_eip.cce.address
 }
 
-data "hcs_vpc_subnet" "mysubnet" {
-  id = var.subnet_id
-}
 
 resource "hcs_vpc_eip" "cce" {
   publicip {
-    type = "5_bgp"
+    type = "eip_external_net"
   }
   bandwidth {
     name        = "cce-apiserver"
     size        = 20
     share_type  = "PER"
-    charge_mode = "traffic"
   }
 }
 
-resource "hcs_cce_cluster" "cluster" {
-  name                   = var.cce_name
-  cluster_type           = "VirtualMachine"
-  cluster_version        = "v1.19"
-  flavor_id              = "cce.s1.small"
-  vpc_id                 = data.hcs_vpc.myvpc.id
-  subnet_id              = data.hcs_vpc_subnet.mysubnet.id
-  container_network_type = "overlay_l2"
-  authentication_mode    = "rbac"
-  eip                    = hcs_vpc_eip.cce.address
-  delete_all             = "true"
-}
-
-resource "hcs_cce_node" "cce-node1" {
-  cluster_id        = hcs_cce_cluster.cluster.id
-  name              = "node1"
-  flavor_id         = "s6.large.2"
-  availability_zone = data.hcs_availability_zones.myaz.names[0]
-  key_pair          = var.key_pair_name
-
-  root_volume {
-    size       = 80
-    volumetype = "SAS"
-  }
-  data_volumes {
-    size       = 100
-    volumetype = "SAS"
-  }
-}
-
-resource "hcs_cce_node" "cce-node2" {
-  cluster_id        = hcs_cce_cluster.cluster.id
-  name              = "node2"
-  flavor_id         = "s6.large.2"
+resource "hcs_cce_node" "cce-node" {
+  cluster_id        = hcs_cce_cluster.mycce.id
+  name              = "node"
+  flavor_id         = "rc6.large.2"
   availability_zone = data.hcs_availability_zones.myaz.names[0]
   key_pair          = var.key_pair_name
 
@@ -68,10 +59,10 @@ resource "hcs_cce_node" "cce-node2" {
 }
 
 provider "kubernetes" {
-  host                   = "https:/${hcs_vpc_eip.cce.address}:5443"
-  cluster_ca_certificate = base64decode(hcs_cce_cluster.cluster.certificate_clusters[0].certificate_authority_data)
-  client_certificate     = base64decode(hcs_cce_cluster.cluster.certificate_users[0].client_certificate_data)
-  client_key             = base64decode(hcs_cce_cluster.cluster.certificate_users[0].client_key_data)
+  host                   = "https://${hcs_vpc_eip.cce.address}:5443"
+  cluster_ca_certificate = base64decode(hcs_cce_cluster.mycce.certificate_clusters[0].certificate_authority_data)
+  client_certificate     = base64decode(hcs_cce_cluster.mycce.certificate_users[0].client_certificate_data)
+  client_key             = base64decode(hcs_cce_cluster.mycce.certificate_users[0].client_key_data)
 }
 
 resource "kubernetes_namespace" "example" {
