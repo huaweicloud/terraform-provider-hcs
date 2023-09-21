45d0630742ac766d3a6d0d40a364682d83721c61
lilei <lilei256@huawei.com>
2023-09-21 15:04:56 +0800

    docs(example/vpc、dns): support a basic example for vpc and dns resource

---
+++ examples/dns/basic/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_name" {
+  default = "vpc-test"
+}
+
+variable "zone_name" {
+  default = "zone-test.com"
+}
+
+variable "record_name" {
+  default = "zone-test.com"
+}
+
+variable "vpc_cidr" {
+  default = "172.16.0.0/16"
+}
