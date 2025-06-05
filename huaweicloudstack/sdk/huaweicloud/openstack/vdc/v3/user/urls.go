1a637d39a9cf70be4bbed06f2486ea97112c28ed
huawei <wanggongyin2@huawei.com>
2025-06-05 15:03:30 +0800

    [ModifyDesc] 支持用户datasource

--- huaweicloudstack/sdk/huaweicloud/openstack/vdc/v3/user/urls.go
+++ huaweicloudstack/sdk/huaweicloud/openstack/vdc/v3/user/urls.go
@@ -2,8 +2,20 @@ package user
 
 import golangsdk "github.com/huaweicloud/terraform-provider-hcs/huaweicloudstack/sdk/huaweicloud"
 
-const resourcePath = "rest/vdc/v3.2/vdcs"
+const resourcePath = "rest/vdc/v3.2"
 
-func rootURL(c *golangsdk.ServiceClient, vdcId string) string {
-	return c.ServiceURL(resourcePath, vdcId, "users")
+func listOrCreateURL(c *golangsdk.ServiceClient, vdcId string) string {
+	return c.ServiceURL(resourcePath, "vdcs", vdcId, "users")
+}
+
+func detailOrPutURL(c *golangsdk.ServiceClient, id string) string {
+	return c.ServiceURL(resourcePath, "users", id)
+}
+
+func pwdURL(c *golangsdk.ServiceClient, id string) string {
+	return c.ServiceURL("rest/vdc/v3.0/users", id, "reset-password")
+}
+
+func deleteURL(c *golangsdk.ServiceClient, id string) string {
+	return c.ServiceURL("rest/vdc/v3.0/users", id)
 }
