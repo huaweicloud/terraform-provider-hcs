66c0ea6bfdf1e8343a7182b0a17c28be69815dfd
huawei <tanpengfei5@huawei.com>
2025-06-16 18:05:22 +0800

    [ModifyDesc]feat(vdc role): add new data source

--- huaweicloudstack/services/acceptance/vdc/data_source_hcs_vdc_role_test.go
+++ huaweicloudstack/services/acceptance/vdc/data_source_hcs_vdc_role_test.go
@@ -7,76 +7,9 @@ import (
 	"testing"
 )
 
-func TestAccVdcRoleDataSource_byNotExistDisplayName(t *testing.T) {
-	notExistName := acceptance.RandomAccVdcRoleDisplayName()
-	dataSourceTestName := "data.hcs_vdc_role.test"
-	dataSourceName := "hcs_vdc_role"
-
-	dc := acceptance.InitDataSourceCheck(dataSourceTestName)
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
-		ProviderFactories: acceptance.TestAccProviderFactories,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccDataSourceVdcRoleByDisplayName(dataSourceName, notExistName),
-				Check: resource.ComposeTestCheckFunc(
-					dc.CheckResourceDestroy(),
-				),
-			},
-		},
-	})
-}
-
-func TestAccVdcRoleDataSource_byExistDisplayName(t *testing.T) {
-	existDisplayName := acceptance.GetExistAccVdcRoleDisplayName()
-	dataSourceTestName := "data.hcs_vdc_role.test"
-	dataSourceName := "hcs_vdc_role"
-
-	dc := acceptance.InitDataSourceCheck(dataSourceTestName)
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
-		ProviderFactories: acceptance.TestAccProviderFactories,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccDataSourceVdcRoleByDisplayName(dataSourceName, existDisplayName),
-				Check: resource.ComposeTestCheckFunc(
-					dc.CheckResourceExists(),
-					resource.TestCheckResourceAttr(dataSourceTestName, "display_name", existDisplayName),
-					acceptance.TestCheckResourceAttrWithVariable(dataSourceTestName, "id", "${hcs_vdc_role.test.id}"),
-					acceptance.TestCheckResourceAttrWithVariable(dataSourceTestName, "name", "${hcs_vdc_role.test.name}"),
-					acceptance.TestCheckResourceAttrWithVariable(dataSourceTestName, "type", "${hcs_vdc_role.test.type}"),
-					acceptance.TestCheckResourceAttrWithVariable(dataSourceTestName, "description", "${hcs_vdc_role.test.description}"),
-					acceptance.TestCheckResourceAttrWithVariable(dataSourceTestName, "policy", "${hcs_vdc_role.test.policy}"),
-				),
-			},
-		},
-	})
-}
-
-func TestAccVdcRoleDataSource_byNotExistName(t *testing.T) {
-	notExistName := acceptance.RandomAccVdcRoleName()
-	dataSourceTestName := "data.hcs_vdc_role.test"
-	dataSourceName := "hcs_vdc_role"
-
-	dc := acceptance.InitDataSourceCheck(dataSourceTestName)
-
-	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
-		ProviderFactories: acceptance.TestAccProviderFactories,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccDataSourceVdcRoleByName(dataSourceName, notExistName),
-				Check: resource.ComposeTestCheckFunc(
-					dc.CheckResourceDestroy(),
-				),
-			},
-		},
-	})
-}
 func TestAccVdcRoleDataSource_byExistName(t *testing.T) {
-	existName := acceptance.GetExistAccVdcRoleName()
+	existName := "readonly"
+
 	dataSourceTestName := "data.hcs_vdc_role.test"
 	dataSourceName := "hcs_vdc_role"
 
@@ -91,11 +24,32 @@ func TestAccVdcRoleDataSource_byExistName(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					dc.CheckResourceExists(),
 					resource.TestCheckResourceAttr(dataSourceTestName, "name", existName),
-					acceptance.TestCheckResourceAttrWithVariable(dataSourceTestName, "id", "${hcs_vdc_role.test.id}"),
-					acceptance.TestCheckResourceAttrWithVariable(dataSourceTestName, "display_name", "${hcs_vdc_role.test.display_name}"),
-					acceptance.TestCheckResourceAttrWithVariable(dataSourceTestName, "type", "${hcs_vdc_role.test.type}"),
-					acceptance.TestCheckResourceAttrWithVariable(dataSourceTestName, "description", "${hcs_vdc_role.test.description}"),
-					acceptance.TestCheckResourceAttrWithVariable(dataSourceTestName, "policy", "${hcs_vdc_role.test.policy}"),
+					resource.TestCheckResourceAttr(dataSourceTestName, "display_name", "Tenant Guest"),
+					resource.TestCheckResourceAttr(dataSourceTestName, "type", "AA"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccVdcRoleDataSource_byExistDisplayName(t *testing.T) {
+	existDisplayName := "Tenant Guest"
+	dataSourceTestName := "data.hcs_vdc_role.test1"
+	dataSourceName := "hcs_vdc_role"
+
+	dc := acceptance.InitDataSourceCheck(dataSourceTestName)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { acceptance.TestAccPreCheck(t) },
+		ProviderFactories: acceptance.TestAccProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceVdcRoleByDisplayName(dataSourceName, existDisplayName),
+				Check: resource.ComposeTestCheckFunc(
+					dc.CheckResourceExists(),
+					resource.TestCheckResourceAttr(dataSourceTestName, "name", "readonly"),
+					resource.TestCheckResourceAttr(dataSourceTestName, "display_name", existDisplayName),
+					resource.TestCheckResourceAttr(dataSourceTestName, "type", "AA"),
 				),
 			},
 		},
@@ -103,17 +57,17 @@ func TestAccVdcRoleDataSource_byExistName(t *testing.T) {
 }
 
 func testAccDataSourceVdcRoleByName(rName, name string) string {
+
 	return fmt.Sprintf(`
 data "%s" "test" {
   name = "%s"
-}
-`, rName, name)
+}`, rName, name)
 }
 
 func testAccDataSourceVdcRoleByDisplayName(rName, displayName string) string {
 	return fmt.Sprintf(`
-data "%s" "test" {
-  display_name = "%s"
+data "%s" "test1" {
+ display_name = "%s"
 }
 `, rName, displayName)
 }
