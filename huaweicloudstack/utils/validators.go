c5a59ca07d0e04aaed6a4801f4c9b70bce3a8d45
huawei <tanpengfei5@huawei.com>
2025-06-28 18:43:57 +0800

    [ModifyDesc] feat(vdc project): add new resource

--- huaweicloudstack/utils/validators.go
+++ huaweicloudstack/utils/validators.go
@@ -224,3 +224,18 @@ func ValidateVBSBackupDescription(v interface{}, k string) (ws []string, errors 
 	}
 	return
 }
+func ValidateVdcProjectInputName(v interface{}, k string) (ws []string, errors []error) {
+	value := v.(string)
+	if len(value) > 64 {
+		errors = append(errors, fmt.Errorf(
+			"%q cannot be longer than 64 characters: %q", k, value))
+	}
+
+	pattern := `^[A-Za-z0-9\\u4e00-\\u9fa5\.\-_]+$`
+	if !regexp.MustCompile(pattern).MatchString(value) {
+		errors = append(errors, fmt.Errorf(
+			"%q doesn't comply with restrictions (%q): %q",
+			k, pattern, value))
+	}
+	return
+}
