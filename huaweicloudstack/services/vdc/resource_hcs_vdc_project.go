89e54b0f5c62ceaf212f69ad3ae4efaf8c2f076d
huawei <tanpengfei5@huawei.com>
2025-07-07 10:49:04 +0800

    [ModifyDesc] feat(vdc project): update data source and resource

--- huaweicloudstack/services/vdc/resource_hcs_vdc_project.go
+++ huaweicloudstack/services/vdc/resource_hcs_vdc_project.go
@@ -47,9 +47,11 @@ func ResourceVdcProject() *schema.Resource {
 				Type:     schema.TypeString,
 				Required: true,
 			},
+
 			"regions": {
-				Type:     schema.TypeString,
+				Type:     schema.TypeList,
 				Computed: true,
+				Elem:     regionsSchema(),
 			},
 			"is_support_hws_service": {
 				Type:     schema.TypeBool,
@@ -58,6 +60,115 @@ func ResourceVdcProject() *schema.Resource {
 			"is_shared": {
 				Type:     schema.TypeBool,
 				Computed: true,
+			},
+		},
+	}
+}
+
+func regionsSchema() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"region_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"region_name": {
+				Type:     schema.TypeMap,
+				Computed: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"region_type": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"region_status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"cloud_infras": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     cloudInfrasSchema(),
+			},
+		},
+	}
+}
+
+func cloudInfrasSchema() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"cloud_infra_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"cloud_infra_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"cloud_infra_status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"cloud_infra_type": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"azs": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     azsSchema(),
+			},
+			"quotas": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     quotasSchema(),
+			},
+		},
+	}
+}
+
+func azsSchema() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"az_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"az_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"az_status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func quotasSchema() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"cloud_infra_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"cloud_infra_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"cloud_infra_status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"cloud_infra_type": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"azs": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     azsSchema(),
 			},
 		},
 	}
@@ -114,10 +225,16 @@ func resourceVdcProjectRead(_ context.Context, schemaResourceData *schema.Resour
 		return common.CheckDeletedDiag(schemaResourceData, err, "VDC project")
 	}
 
+	elements := getRegionElements(*project)
+
 	mErr := multierror.Append(nil,
 		schemaResourceData.Set("name", project.Name),
 		schemaResourceData.Set("display_name", project.DisplayName),
 		schemaResourceData.Set("description", project.Description),
+		schemaResourceData.Set("is_support_hws_service", project.IsSupportHwsService),
+		schemaResourceData.Set("is_shared", project.IsShared),
+		schemaResourceData.Set("regions", elements),
+		schemaResourceData.Set("vdc_id", project.TenantId),
 	)
 	if err = mErr.ErrorOrNil(); err != nil {
 		return diag.Errorf("Error setting VDC project fields: %s", err)
@@ -125,6 +242,91 @@ func resourceVdcProjectRead(_ context.Context, schemaResourceData *schema.Resour
 
 	return nil
 }
+
+func getRegionElements(project sdk.QueryProjectDetailV31) []map[string]interface{} {
+	elements := make([]map[string]interface{}, 0, len(project.Regions))
+	for _, group := range project.Regions {
+		groupMap := map[string]interface{}{
+			"region_id":     group.RegionId,
+			"region_name":   getRegionName(group.RegionName),
+			"region_type":   group.RegionType,
+			"region_status": group.RegionStatus,
+			"cloud_infras":  getCloudInfras(group.CloudInfras),
+		}
+
+		elements = append(elements, groupMap)
+	}
+	return elements
+}
+
+func getRegionName(regionName sdk.RegionName) map[string]string {
+	element := make(map[string]string)
+	element["en_us"] = regionName.EnUs
+	element["zh_cn"] = regionName.ZhCn
+	return element
+}
+
+func getCloudInfras(cloudInfras []sdk.ProjectCloudInfraInfo) []map[string]interface{} {
+	elements := make([]map[string]interface{}, 0, len(cloudInfras))
+	for _, group := range cloudInfras {
+		groupMap := map[string]interface{}{
+			"cloud_infra_id":     group.CloudInfraId,
+			"cloud_infra_name":   group.CloudInfraName,
+			"cloud_infra_type":   group.CloudInfraType,
+			"cloud_infra_status": group.CloudInfraStatus,
+			"azs":                getAzs(group.Azs),
+			"quotas":             getQuotas(group.Quotas),
+		}
+
+		elements = append(elements, groupMap)
+	}
+	return elements
+}
+
+func getAzs(azs []sdk.ProjectAzInfo) []map[string]interface{} {
+	elements := make([]map[string]interface{}, 0, len(azs))
+	for _, group := range azs {
+		groupMap := map[string]interface{}{
+			"az_id":     group.AzId,
+			"az_name":   group.AzName,
+			"az_status": group.AzStatus,
+		}
+
+		elements = append(elements, groupMap)
+	}
+	return elements
+}
+
+func getQuotas(quotas []sdk.QuotaInfo) []map[string]interface{} {
+	elements := make([]map[string]interface{}, 0, len(quotas))
+	for _, group := range quotas {
+		groupMap := map[string]interface{}{
+			"service_id": group.ServiceId,
+			"action":     group.Action,
+			"resources":  getDictionaryList(group.Resources),
+		}
+
+		elements = append(elements, groupMap)
+	}
+	return elements
+}
+
+func getDictionaryList(dictList []sdk.DictInfo) []map[string]interface{} {
+	elements := make([]map[string]interface{}, 0, len(dictList))
+	for _, group := range dictList {
+		groupMap := map[string]interface{}{
+			"resource":    group.Resource,
+			"local_limit": group.LocalLimit,
+			"other_limit": group.OtherLimit,
+			"local_used":  group.LocalUsed,
+			"other_used":  group.OtherUsed,
+		}
+
+		elements = append(elements, groupMap)
+	}
+	return elements
+}
+
 func resourceVdcProjectUpdate(ctx context.Context, schemaResourceData *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	configContext := config.GetHcsConfig(meta)
 	region := configContext.GetRegion(schemaResourceData)
